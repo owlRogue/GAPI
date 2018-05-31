1f98e550d31351ae379478ac1f6ea8b077e947fb
Koltyn Palmer <koltynchiaro@gmail.com>
2018-05-31 02:43:33 -0700

    v1 unpolished

--- credentials.js
+++
@@ -1,9 +0,0 @@
-var credentials = {
-    gapi_Key: 'AIzaSyCAHkUP1S8LPnopLf3juH34O5l-iJWUdL8',
-    gapi_Client: '636126207964-eu6thv1vbffi8g7t0smvstctlgmmb6t6.apps.googleusercontent.com'
-}
-
-var sheet = {
-    sheet_ID: '1l1GyTc65N-afxhIJ-9O7eogKYdOIUvZQw1m1D9IWoyQ',
-    sheet_range: ['A1:Q100']
-}

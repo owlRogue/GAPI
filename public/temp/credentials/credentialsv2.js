29d1909d7d409a73e6ad067ecf0ae4cb83c496f7
Koltyn Palmer <palmer@ring.com>
2018-07-18 20:48:49 -0700

    running

---
+++ public/temp/credentials/credentialsv2.js
@@ -0,0 +1,13 @@
+// Google Credentials:
+
+module.exports = {
+    gapi_Key: "AIzaSyDmahkvaiX7_UfvhIeneSkESNKR4MhDRbM", // KCH@GM
+    // gapi_Key = "AIzaSyDmahkvaiX7_UfvhIeneSkESNKR4MhDRbM", // palmer
+    // gapi_Key: "AIzaSyCbR_nmon6VsFaaCHti0s-ECRSw3jlrzd4", // cs
+    // gapi_Key: "", // cs
+    // gapi_Key: "7d26a646dc38659a5f669c1b68d58b240c5aa633", // service
+    gapi_Client: "636126207964-eu6thv1vbffi8g7t0smvstctlgmmb6t6.apps.googleusercontent.com" // KCH@gmail
+    // gapi_Client = "291977590444-gsu34sc0jorph142r1ajmdo24sb7jab0.apps.googleusercontent.com" // palmer
+    // gapi_Client: "876864941121-gr57qs2lcfq8f4un9i1rsmcu53v3rivg.apps.googleusercontent.com" // cs
+    // gapi_Client: "113754371320132409517" // service
+};

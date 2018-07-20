94c7a231c199efe16cf779753fb601a2b81f53ac
Koltyn Palmer <palmer@ring.com>
2018-07-19 21:10:49 -0600

    Sheets.js is now cred file

--- public/v2/credentials/Sheets.template.js
+++ public/v2/credentials/Sheets.template.js
@@ -1,6 +1,5 @@
 // INSTRUCTIONS :
-// Replace dummyData.js (or) SCsheetsIDs.js with the below. 
-// Only one of those 2 files should be active.
+// Retitle to Sheets.js
 
 // var sheet_ID;
 // var sheet_range;
@@ -8,12 +7,12 @@
 
 // var sheet = [
 //   (defaultSheet = {
-//     sheet_ID: '1l1GyTc65N-afxhIJ-9O7eogKYdOIUvZQw1m1D9IWoyQ',
+//     sheet_ID: '1l1GyTc65N-afxhIJ-9O7eogKYdOIUvZQw1m1D9IWoyQ', // public sheet
 //     sheet_range: ['A1:Q100'],
 //     sheet_title: 'dummy data'
 //   }),
 //   (sheet1 = {
-//     sheet_ID: '1KbuidBzpeH_i6X-1QoXgxcyi2PoldMyiBtqvRX_OuAQ',
+//     sheet_ID: '1KbuidBzpeH_i6X-1QoXgxcyi2PoldMyiBtqvRX_OuAQ', // public sheet
 //     sheet_range: ['A1:ZZ200'],
 //     sheet_title: 'Data Bunch 1'
 //   }),

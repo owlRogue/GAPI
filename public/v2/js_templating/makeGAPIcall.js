4d2171affe2304ab48850efb55b761f016e4ab97
Koltyn Palmer <palmer@ring.com>
2018-07-21 13:47:45 -0600

    fixing

--- public/v2/js_templating/makeGAPIcall.js
+++ public/v2/js_templating/makeGAPIcall.js
@@ -1,29 +1,29 @@
-// Make API call to gSheets:
-function makeApiCall() {
-  $("#instructions")
-    .empty()
-    .removeClass("instructions");
+// // Make API call to gSheets:
+// function makeApiCall() {
+//   $("#instructions")
+//     .empty()
+//     .removeClass("instructions");
 
-  const params = {
-    spreadsheetId: sheet_ID,
-    range: sheet_range,
-    valueRenderOption: "FORMATTED_VALUE",
-    dateTimeRenderOption: "SERIAL_NUMBER"
-  };
+//   const params = {
+//     spreadsheetId: sheet_ID,
+//     range: sheet_range,
+//     valueRenderOption: "FORMATTED_VALUE",
+//     dateTimeRenderOption: "SERIAL_NUMBER"
+//   };
 
-  const request = gapi.client.sheets.spreadsheets.values.get(params);
+//   const request = gapi.client.sheets.spreadsheets.values.get(params);
 
-  request.then(function(response) {
-    renderTable(response.result.values);
-  });
-};
+//   request.then(function(response) {
+//     renderTable(response.result.values);
+//   });
+// };
 
-// Set landing page to Legend:
-function landingPage() {
-  sheet_ID = defaultSheet.sheet_ID;
-  sheet_range = defaultSheet.sheet_range;
-  makeApiCall();
-};
+// // Set landing page to Legend:
+// function landingPage() {
+//   sheet_ID = defaultSheet.sheet_ID;
+//   sheet_range = defaultSheet.sheet_range;
+//   makeApiCall();
+// };
 
-// ERROR CHECK FEATURE: logs file loaded to console
-logFile();
+// // ERROR CHECK FEATURE: logs file loaded to console
+// logFile();

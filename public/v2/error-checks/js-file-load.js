75bce625dea66807b0d4e8f46f7e879d7928414d
Adam Miner <miner.adam1@hotmail.com>
2018-07-21 11:52:50 -0600

    Tweaks

--- public/v2/error-checks/js-file-load.js
+++ public/v2/error-checks/js-file-load.js
@@ -3,11 +3,9 @@ var fileString;
 
 var logFile = () => {
     var filePath;
-    (() => {
-        const scripts = document.getElementsByTagName('script');
-        filePath = scripts[scripts.length - 1].src;
-    });
-        // console.log("file path:" + "\n" + filePath); // whole path
+    const scripts = document.getElementsByTagName('script');
+    filePath = scripts[scripts.length - 1].src;
+    // console.log("file path:" + "\n" + filePath); // whole path
     
     localhost = filePath.substr(0, 21); // isolates "http://localhost:8000/js/"
         // console.log("localhost" + "\n" + localhost);

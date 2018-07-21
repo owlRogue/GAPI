b306933e9e907135829cfa6c4d587c0022f28b42
Myeager86 <31748384+Myeager86@users.noreply.github.com>
2018-07-21 10:46:44 -0600

    pushing

--- public/v3/react-app/client/src/components/Table/Table.js
+++ public/v3/react-app/client/src/components/Table/Table.js
@@ -1,5 +1,5 @@
-// import React from "react";
-// import { Table } from "react-router-dom";
+import React from "react";
+import { Table } from "react-router-dom";
 
 // // usage: 2D array, first element = column name, second element: property of object in data array
 // // order added = order of columns presented in the table
@@ -9,3 +9,55 @@
 // <Table columns={''} data={''}/>;
 
 // export default Table;
+
+const styles = theme => ({
+    root: {
+      width: '100%',
+      marginTop: theme.spacing.unit * 3,
+      overflowX: 'auto',
+    },
+    table: {
+      minWidth: 700,
+    },
+  });
+
+let id = 0;
+function createData(name, calories, fat, carbs, protein) {
+  id += 1;
+  return { id, name, calories, fat, carbs, protein };
+}
+
+function SimpleTable(props) {
+    const { classes } = props;
+  
+    return (
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>Dessert (100g serving)</TableCell>
+              <TableCell numeric>Calories</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data.map(n => {
+              return (
+                <TableRow key={n.id}>
+                  <TableCell component="th" scope="row">
+                    {n.name}
+                  </TableCell>
+                  <TableCell numeric>{n.calories}</TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </Paper>
+    );
+  }
+  
+  SimpleTable.propTypes = {
+    classes: PropTypes.object.isRequired,
+  };
+  
+  export default withStyles(styles)(SimpleTable);
